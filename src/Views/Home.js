b767b1d3510e8cedb52106c2219af9db86c9483b
WilsonChen40172 <40795429+WilsonChen40172@users.noreply.github.com>
2022-04-16 14:22:01 +0800

    add images

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from "react";
+// import wilson1 from "../images/wilson_home_1.jpg"
+import wilson_home_1 from "../images/wilson_home_1.JPG";
+import wilson_home_2 from "../images/wilson_home_2.JPG";
+import wilson_home_3 from "../images/wilson_home_3.JPG";
+import wilson_home_4 from "../images/wilson_home_4.JPG";
+import wilson_home_5 from "../images/wilson_home_5.JPG";
+import style from "./Home.module.scss";
 
 const Home = () => {
   const [img, setImg] = useState(0);
@@ -12,12 +19,34 @@ const Home = () => {
 
   return (
     <div className="Home">
-      <div className="section">
-        <div className="imgWall">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+      <div className={"imgWall d-flex " + style.imgWall}>
+        <div className={"col-6 d-flex " + style.onePicWall}>
+          <div className={"col-12 "+style.picCard}>
+            <img className="" src={wilson_home_1} alt="wilson1" />
+            <p><a href="">111</a></p>
+          </div>
+        </div>
+        <div className={"col-6 d-flex " + style.fourPicsWall}>
+          <div className="col-6">
+            <div className={"col-12 "+style.picCard}>
+              <img className="" src={wilson_home_2} alt="wilson2" />
+              <p><a href="">123</a></p>
+            </div>
+            <div className={"col-12 "+style.picCard}>
+              <img className="" src={wilson_home_3} alt="wilson3" />
+              <p><a href="">123</a></p>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className={"col-12 "+style.picCard}>
+              <img className="" src={wilson_home_4} alt="wilson4" />
+              <p><a href="">123</a></p>
+            </div>
+            <div className={"col-12 "+style.picCard}>
+              <img className="" src={wilson_home_5} alt="wilson5" />
+              <p><a href="">123</a></p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="section">

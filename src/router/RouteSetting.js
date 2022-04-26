caf88f8ac9c0dc3de7e35d3a97e1c84149e41328
WilsonChen40172 <40795429+WilsonChen40172@users.noreply.github.com>
2022-04-26 12:13:12 +0800

    setting new views

--- src/router/RouteSetting.js
+++ src/router/RouteSetting.js
@@ -1,10 +1,10 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 // import GlobalMethod from "../global/GlobalMethod";
-import Home from "../Views/Home";
-import Stock from "../Views/Stock";
-import F2E from "../Views/F2E";
-import NotFound from "../Views/NotFound";
+import Home from "../views/Home";
+import Stock from "../views/Stock";
+import F2E from "../views/F2E";
+import NotFound from "../views/NotFound";
 import Header from "../conponents/Header";
 import Navbar from "../conponents/Navbar";
 

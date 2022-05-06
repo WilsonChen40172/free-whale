93014502b0f2474b6b02a9bda7a3d6fa70fa9ade
WilsonChen40172 <40795429+WilsonChen40172@users.noreply.github.com>
2022-05-06 16:34:10 +0800

    header ipad

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-// import logo from "../images/free_whale_logo.jpeg";
 // import GlobalMethod from "../global/GlobalMethod";
 import { Link } from "react-router-dom";
 import style from "./Header.module.scss";
@@ -7,11 +6,6 @@ const Header = () => {
     <section className={style.header}>
       <div className={style.headerList}>
         <ul className="">
-          {/* <li className={style.logo}>
-            <Link to={"/"}>
-              <img src={logo} alt="" />
-            </Link>
-          </li> */}
           <li>
             <Link to={"/"}>Home</Link>
           </li>
@@ -21,10 +15,6 @@ const Header = () => {
           <li>
             <Link to={"/F2E"}>F2E</Link>
           </li>
-
-          <li>{/* <a href="/">舞蹈</a> */}</li>
-          <li>{/* <a href="/">健身</a> */}</li>
-          <li>{/* <a href="/">滑板</a> */}</li>
         </ul>
       </div>
     </section>
